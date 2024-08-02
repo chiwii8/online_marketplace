7f628e9638f867e51265164976dbb441dc83f70e
Chiwii8 <alejandrosanrod@outlook.com>
2024-08-02 13:47:31 +0200

    new: Create the project

---
+++ src/main/java/app/MarketPlaceAplication.java
@@ -0,0 +1,12 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketPlaceAplication {
+    public static void main(String [] args) {
+        SpringApplication.run(MarketPlaceAplication.class, args);
+    }
+
+}
