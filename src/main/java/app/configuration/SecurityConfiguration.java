2f953cb742ad7ad05712dca9f4b79ce7a960883e
Chiwii8 <alejandrosanrod@outlook.com>
2025-02-12 21:43:14 +0100

    update: disable csrf to check in postman

--- src/main/java/app/configuration/SecurityConfiguration.java
+++ src/main/java/app/configuration/SecurityConfiguration.java
@@ -1,19 +1,29 @@
 package app.configuration;
 
+import app.domain.actor.account.UserAccount;
+import app.security.Authority;
 import app.utils.HashPassword;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+import java.util.function.UnaryOperator;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                                 .requestMatchers("/login","/product/**").permitAll()
                                 .anyRequest().authenticated()
@@ -25,6 +35,7 @@ public class SecurityConfiguration {
 
         return http.build();
     }
+
 
 
     @Bean
